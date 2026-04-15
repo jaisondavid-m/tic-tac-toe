9b5dfbde4fdf71ce0d5eb2544d1bb97f2eaab886
jaisondavid-m <jaisondavidm.cs25@bitsathy.ac.in>
2026-04-15 21:18:09 +0530

    added render.yaml file

--- server/main.go
+++ server/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 	"server/routes"
+	"strings"
 	"time"
 
 	"github.com/gin-contrib/cors"
@@ -18,20 +19,30 @@ func main() {
 	if err != nil {
 		log.Println("No .env file Found")
 	}
-	frontendURL := os.Getenv("FRONTEND_URL")
-	if frontendURL == "" {
-		frontendURL = "http://localhost:5173"
+	frontendURL := strings.TrimSpace(os.Getenv("FRONTEND_URL"))
+	allowedOrigins := []string{"http://localhost:5173"}
+	if frontendURL != "" {
+		allowedOrigins = []string{}
+		for _, rawOrigin := range strings.Split(frontendURL, ",") {
+			origin := strings.TrimSpace(strings.TrimSuffix(rawOrigin, "/"))
+			if origin != "" {
+				allowedOrigins = append(allowedOrigins, origin)
+			}
+		}
+		if len(allowedOrigins) == 0 {
+			allowedOrigins = []string{"http://localhost:5173"}
+		}
 	}
 	gin.SetMode(gin.ReleaseMode)
 	// r := gin.Default()
 	r := gin.New()
 	r.Use(gin.Recovery())
 	r.Use(cors.New(cors.Config{
-		AllowOrigins: []string{frontendURL},
-		AllowMethods: []string{"GET","POST","PUT","DELETE","OPTIONS"},
-		AllowHeaders: []string{"Origin","Content-Type","Authorization"},
+		AllowOrigins:     allowedOrigins,
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		AllowCredentials: true,
-		MaxAge: 12*time.Hour,
+		MaxAge:           12 * time.Hour,
 	}))
 	routes.TestRoutes(r)
 	routes.AuthRoutes(r)
