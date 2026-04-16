3237a2ee244ce25e579dda851747f2f963ba6965
jaisondavid-m <jaisondavidm.cs25@bitsathy.ac.in>
2026-04-16 07:02:58 +0530

    temperorialy remove login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import BotGame from "./Pages/BotGame.jsx"
 import Home from "./Pages/Home.jsx"
 import Login from "./Pages/Login.jsx"
-import { AuthProvider } from "./context/AuthContext.jsx"
+// import { AuthProvider } from "./context/AuthContext.jsx"
 import ProtectedRoute from "./routes/ProtectedRoute.jsx"
 import ProtectedLayout from "./Layout/ProtectedLayout.jsx"
 import PublicRoute from "./routes/PublicRoute.jsx"
@@ -17,25 +17,29 @@ import BotPage from "./Pages/BotPage.jsx"
 // import WebSocketTest from "./components/WebSocketTest.jsx"
 import Multiplayer from "./Pages/Multiplayer.jsx"
 import CustomCursor from "./components/CustomCursor.jsx"
+import NavBar from "./components/NavBar.jsx"
 
 export default function App() {
   return (
     <BrowserRouter>
-      <AuthProvider>
+      {/* <AuthProvider> */}
         <CustomCursor/>
+        <NavBar/>
         <Routes>
-          <Route path="/" element={<PublicRoute><Login /></PublicRoute>} />
-          <Route path="/github/callback" element={<PublicRoute><GitHubCallback/></PublicRoute>} />
-          <Route path="/hackclub/callback" element={<PublicRoute><HackClubCallback/></PublicRoute>} />
-          <Route
+          {/* <Route path="/" element={<PublicRoute><Login /></PublicRoute>} /> */}
+          {/* <Route path="/github/callback" element={<PublicRoute><GitHubCallback/></PublicRoute>} /> */}
+          {/* <Route path="/hackclub/callback" element={<PublicRoute><HackClubCallback/></PublicRoute>} /> */}
+          {/* <Route
             element={
               <ProtectedRoute>
                 <ProtectedLayout />
               </ProtectedRoute>
             }
-          >
+          > */}
+          <Route>
             {/* <Route path="/websocketdemo" element={<WebSocketTest/>} /> */}
-            <Route path="/profile" element={<Profile/>} />
+            <Route path="/" element={<Home/>}/>
+            {/* <Route path="/profile" element={<Profile/>} /> */}
             <Route path="/home" element={<Home />} />
             <Route path="/bot" element={<BotPage/>} />
             <Route path="/normalbot" element={<BotGame />} />
@@ -46,7 +50,7 @@ export default function App() {
 
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </AuthProvider>
+      {/* </AuthProvider> */}
     </BrowserRouter>
   )
 }
